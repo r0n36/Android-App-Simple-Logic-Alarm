93d9dd43a7c713e75f81724782b104e5148fe103
Mahbub Rony <mahbubur@nascenia.com>
2014-09-03 02:08:18 +0600

    Database integration

---
+++ app/src/main/java/com/example/rony36/simplelogicalarm/Alarm.java
@@ -0,0 +1,192 @@
+package com.example.rony36.simplelogicalarm;
+
+import android.text.format.Time;
+
+import java.lang.String;
+
+public class Alarm {
+
+    //private variables
+    int _id;
+    String _alarm_time;
+    int _status;
+    int _repeat;
+    int _sun;
+    int _mon;
+    int _tue;
+    int _wed;
+    int _thu;
+    int _fri;
+    int _sat;
+    int _urgency;
+    int _off_method;
+    String _ringtone_path;
+    String _note;
+
+    // Empty constructor
+    public Alarm(){
+
+    }
+    // constructor
+    public Alarm(int id,
+                 String alarm_time,
+                 int _status,
+                 int _repeat,
+                 int _sun,
+                 int _mon,
+                 int _tue,
+                 int _wed,
+                 int _thu,
+                 int _fri,
+                 int _sat,
+                 int _urgency,
+                 int _off_method,
+                 String _ringtone_path,
+                 String _note){
+
+        this._id            = id;
+        this._alarm_time    = alarm_time;
+        this._status        = _status;
+        this._repeat        = _repeat;
+        this._sun           = _sun;
+        this._mon           = _mon;
+        this._tue           = _tue;
+        this._wed           = _wed;
+        this._thu           = _thu;
+        this._fri           = _fri;
+        this._sat           = _sat;
+        this._urgency       = _urgency;
+        this._off_method    = _off_method;
+        this._ringtone_path = _ringtone_path;
+        this._note          = _note;
+    }
+
+    // constructor
+//    public Alarm(String name, String _phone_number){
+//        this._name = name;
+//        this._phone_number = _phone_number;
+//    }
+    // getting ID
+    public int get_id(){
+        return this._id;
+    }
+
+    // setting id
+    public void set_id(int id){
+        this._id = id;
+    }
+
+    // getting name
+    public String get_alarm_time(){
+        return this._alarm_time;
+    }
+
+    // setting name
+    public void set_alarm_time(String alarm_time){
+        this._alarm_time = alarm_time;
+    }
+
+    public int get_status(){
+        return this._status;
+    }
+
+    public void set_status(int status){
+        this._status = status;
+    }
+
+    public int get_repeat(){
+        return this._repeat;
+    }
+
+    public int get_sun(){
+        return this._sun;
+    }
+
+    public int get_mon(){
+        return this._mon;
+    }
+
+    public int get_tue(){
+        return this._tue;
+    }
+
+    public int get_wed(){
+        return this._wed;
+    }
+
+    public int get_thu(){
+        return this._thu;
+    }
+
+    public int get_fri(){
+        return this._fri;
+    }
+
+    public int get_sat(){
+        return this._sat;
+    }
+
+    public int get_urgency(){
+        return this._urgency;
+    }
+
+    public int get_off_method(){
+        return this._off_method;
+    }
+
+    public String get_ringtone_path(){
+        return this._ringtone_path;
+    }
+
+    public String get_note(){
+        return this._note;
+    }
+
+    public void set_repeat(int repeat){
+        this._repeat = repeat;
+    }
+
+    public void set_sun(int sun){
+        this._sun = sun;
+    }
+
+    public void set_mon(int mon){
+        this._mon = mon;
+    }
+
+    public void set_tue(int tue){
+        this._tue = tue;
+    }
+
+    public void set_wed(int wed){
+        this._wed = wed;
+    }
+
+    public void set_thu(int thu){
+        this._thu = thu;
+    }
+
+    public void set_fri(int fri){
+        this._fri = fri;
+    }
+
+    public void set_sat(int sat){
+        this._sat = sat;
+    }
+
+    public void set_urgency(int urgency){
+        this._urgency = urgency;
+    }
+
+    public void set_off_method(int off_method){
+        this._off_method = off_method;
+    }
+
+    public void set_ringtone_path(String ringtone_path){
+        this._ringtone_path = ringtone_path;
+    }
+
+    public void set_note(String note){
+        this._note = note;
+    }
+}
---
+++ app/src/main/java/com/example/rony36/simplelogicalarm/DatabaseHandler.java
@@ -0,0 +1,212 @@
+package com.example.rony36.simplelogicalarm;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+//import android.text.format.Time;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by RonyLap on 9/2/2014.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "simpleAlarm";
+    private static final String TABLE_LOGIC_ALARM = "logicAlarm";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_TIME = "alarm_time";
+    private static final String KEY_STATUS = "status";
+    private static final String KEY_REPEAT = "repeat";
+    private static final String KEY_SUN = "sun";
+    private static final String KEY_MON = "mon";
+    private static final String KEY_TUE = "tue";
+    private static final String KEY_WED = "wed";
+    private static final String KEY_THU = "thu";
+    private static final String KEY_FRI = "fri";
+    private static final String KEY_SAT = "sat";
+    private static final String KEY_URGENCY = "urgency";
+    private static final String KEY_OFF_METHOD = "off_method";
+    private static final String KEY_RINGTONE = "ringtone_path";
+    private static final String KEY_NOTE = "note";
+
+    public DatabaseHandler(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_LOGIC_ALARM_TABLE = "CREATE TABLE " + TABLE_LOGIC_ALARM + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_TIME + " TEXT,"
+                + KEY_STATUS + " INTEGER DEFAULT 0,"
+                + KEY_REPEAT + " INTEGER DEFAULT 0,"
+                + KEY_SUN + " INTEGER DEFAULT 0,"
+                + KEY_MON + " INTEGER DEFAULT 0,"
+                + KEY_TUE + " INTEGER DEFAULT 0,"
+                + KEY_WED + " INTEGER DEFAULT 0,"
+                + KEY_THU + " INTEGER DEFAULT 0,"
+                + KEY_FRI + " INTEGER DEFAULT 0,"
+                + KEY_SAT + " INTEGER DEFAULT 0,"
+                + KEY_URGENCY + " INTEGER DEFAULT -1,"
+                + KEY_OFF_METHOD + " INTEGER DEFAULT -1,"
+                + KEY_RINGTONE + " TEXT"
+                + KEY_NOTE + " TEXT"+
+                ")";
+        db.execSQL(CREATE_LOGIC_ALARM_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOGIC_ALARM);
+
+        // Create tables again
+        onCreate(db);
+    }
+
+    // Adding new alarm
+    void addAlarm(Alarm alarm) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_TIME,        alarm.get_alarm_time());
+        values.put(KEY_STATUS,      alarm.get_status());
+        values.put(KEY_REPEAT,      alarm.get_repeat());
+        values.put(KEY_SUN,         alarm.get_sun());
+        values.put(KEY_MON,         alarm.get_mon());
+        values.put(KEY_TUE,         alarm.get_thu());
+        values.put(KEY_WED,         alarm.get_wed());
+        values.put(KEY_THU,         alarm.get_thu());
+        values.put(KEY_FRI,         alarm.get_fri());
+        values.put(KEY_SAT,         alarm.get_sat());
+        values.put(KEY_URGENCY,     alarm.get_urgency());
+        values.put(KEY_OFF_METHOD,  alarm.get_off_method());
+        values.put(KEY_RINGTONE,    alarm.get_ringtone_path());
+        values.put(KEY_NOTE,        alarm.get_note());
+
+        // Inserting Row
+        db.insert(TABLE_LOGIC_ALARM, null, values);
+        db.close(); // Closing database connection
+    }
+
+    // Getting single alarm
+    Alarm getAlarm(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_LOGIC_ALARM, new String[] { KEY_ID, KEY_TIME,
+                        KEY_STATUS, KEY_REPEAT, KEY_SUN, KEY_MON,
+                        KEY_TUE, KEY_WED, KEY_THU, KEY_FRI, KEY_SAT,
+                        KEY_URGENCY, KEY_OFF_METHOD, KEY_RINGTONE, KEY_NOTE, }, KEY_ID + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Alarm alarm = new Alarm(
+                Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1),
+                Integer.parseInt(cursor.getString(2)),
+                Integer.parseInt(cursor.getString(3)),
+                Integer.parseInt(cursor.getString(4)),
+                Integer.parseInt(cursor.getString(5)),
+                Integer.parseInt(cursor.getString(6)),
+                Integer.parseInt(cursor.getString(7)),
+                Integer.parseInt(cursor.getString(8)),
+                Integer.parseInt(cursor.getString(9)),
+                Integer.parseInt(cursor.getString(10)),
+                Integer.parseInt(cursor.getString(11)),
+                Integer.parseInt(cursor.getString(12)),
+                cursor.getString(13),
+                cursor.getString(14)
+        );
+        // return alarm
+        return alarm;
+    }
+
+    // Getting All Alarms
+    public List<Alarm> getAllAlarms() {
+        List<Alarm> alarmList = new ArrayList<Alarm>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_LOGIC_ALARM;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Alarm alarm = new Alarm();
+                alarm.set_id(Integer.parseInt(cursor.getString(0)));
+                alarm.set_alarm_time(cursor.getString(1));
+                alarm.set_status(Integer.parseInt(cursor.getString(2)));
+                alarm.set_repeat(Integer.parseInt(cursor.getString(3)));
+                alarm.set_sun(Integer.parseInt(cursor.getString(4)));
+                alarm.set_mon(Integer.parseInt(cursor.getString(5)));
+                alarm.set_tue(Integer.parseInt(cursor.getString(6)));
+                alarm.set_wed(Integer.parseInt(cursor.getString(7)));
+                alarm.set_thu(Integer.parseInt(cursor.getString(8)));
+                alarm.set_fri(Integer.parseInt(cursor.getString(9)));
+                alarm.set_sat(Integer.parseInt(cursor.getString(10)));
+                alarm.set_urgency(Integer.parseInt(cursor.getString(11)));
+                alarm.set_off_method(Integer.parseInt(cursor.getString(12)));
+                alarm.set_ringtone_path(cursor.getString(13));
+                alarm.set_note(cursor.getString(14));
+                // Adding alarm to list
+                alarmList.add(alarm);
+            } while (cursor.moveToNext());
+        }
+
+        // return alarm list
+        return alarmList;
+    }
+
+    // Updating single alarm
+    public int updateAlarm(Alarm alarm) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_TIME,        alarm.get_alarm_time());
+        values.put(KEY_STATUS,      alarm.get_status());
+        values.put(KEY_REPEAT,      alarm.get_repeat());
+        values.put(KEY_SUN,         alarm.get_sun());
+        values.put(KEY_MON,         alarm.get_mon());
+        values.put(KEY_TUE,         alarm.get_thu());
+        values.put(KEY_WED,         alarm.get_wed());
+        values.put(KEY_THU,         alarm.get_thu());
+        values.put(KEY_FRI,         alarm.get_fri());
+        values.put(KEY_SAT,         alarm.get_sat());
+        values.put(KEY_URGENCY,     alarm.get_urgency());
+        values.put(KEY_OFF_METHOD,  alarm.get_off_method());
+        values.put(KEY_RINGTONE,    alarm.get_ringtone_path());
+        values.put(KEY_NOTE,        alarm.get_note());
+
+        // updating row
+        return db.update(TABLE_LOGIC_ALARM, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(alarm.get_id()) });
+    }
+
+    // Deleting single alarm
+    public void deleteAlarm(Alarm alarm) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_LOGIC_ALARM, KEY_ID + " = ?",
+                new String[] { String.valueOf(alarm.get_id()) });
+        db.close();
+    }
+
+
+    // Getting alarms Count
+    public int getAlarmsCount() {
+        String countQuery = "SELECT  * FROM " + TABLE_LOGIC_ALARM;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+
+        // return count
+        return cursor.getCount();
+    }
+
+}
